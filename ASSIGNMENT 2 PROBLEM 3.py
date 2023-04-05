9cc6f357dd858586a83d1b34459c879d1b7da2d6
Kristine Joy Barrina <barrinakristinejoy@gmail.com>
2023-04-05 11:15:33 +0800

    Second commit

--- ASSIGNMENT 2 PROBLEM 3.py
+++ ASSIGNMENT 2 PROBLEM 3.py
@@ -1,1 +1,17 @@
+#Kristine Joy Barrina #BSCPE 1-5 #April 4, 2023
 
+# PSEUDOCODE
+
+# Let the user input the message (plaintext)
+# Display an error message when the user inputs a space in plaintext and let them input again
+# Display an error messsage when the user inputs a lowercase in plaintext and let them input again
+# Let the user input the key
+# Display an error message when the user inputs a space in key and let them input again
+# Display an error messsage when the user inputs a lowercase in key and let them input again
+# Convert the character of message to its integer equivalent by using ord
+# Convert the character of key to its integer equivalent by using ord
+# Define a function called encrypt
+# Create a variable with empty strings
+# Add numbers then take the result of mod 26
+# Add 65 to get the ciphertext
+# Call the function and display the output
