5d83d1279b28c41a4be77c338102917ca573f302
Kristine Joy Barrina <barrinakristinejoy@gmail.com>
2023-04-05 12:37:45 +0800

    Fourth commit

--- ASSIGNMENT 2 PROBLEM 3.py
+++ ASSIGNMENT 2 PROBLEM 3.py
@@ -16,7 +16,8 @@ while loop_ctrl_plaintext == 0:
     
 #   Let the user input a message
 
-    plaintext = str (input ("Enter the message: "))
+    plaintext = str (input ("\033[39m" + "Enter the message: "))
+    plaintext_length = len (plaintext)
 
 #   Display an error message when there's a space in message
 
@@ -34,18 +35,19 @@ while loop_ctrl_plaintext == 0:
             loop_ctrl_plaintext += 1
             
         else:
-            print ("Input the message in uppercase!")
+            print ("\033[1;31m" + "Input the message in uppercase!\n")
             loop_ctrl_plaintext = 0
             
     else:
-        print ("Input the message with no spaces!")
+        print ("\033[1;31m" + "Input the message with no spaces!\n")
         loop_ctrl_plaintext = 0
         
 # Let the user input the key
 
 while loop_ctrl_key == 0:
     
-    key = str (input ("Enter the key: "))
+    key = str (input ("\033[39m" +"Enter the key: "))
+    key_length = len (key)
  
  #   Display an error message when there's a space in key   
  
@@ -59,21 +61,45 @@ while loop_ctrl_key == 0:
         
         if key.isupper() == True:
             loop_ctrl_key += 1
-            
+
+#    Display an error message when there's a lowercase in key
+
         else:
-            print ("Input the key in uppercase!") 
+            print ("\033[1;31m" + "Input the key in uppercase!\n") 
             loop_ctrl_key = 0
     else:
-        print ("Input the key with no spaces!")
+        print ("\033[1;31m" + "Input the key with no spaces!\n")
         loop_ctrl_key = 0
         
-# Display an error message when there's a lowercase in key
-# Convert the character of plaintext to its integer equivalent using ord 
+# Convert the character of plaintext to its integer equivalent using ord
+
+for i in range (plaintext_length):
+    plaintext_char_value.append (ord (plaintext [i]))
+ 
 # Convert the character of key to its integer equivalent using ord
+
+for i in range (key_length):
+    key_char_value. append (ord (plaintext [i]))
+
 # Define a function called "encrypt"
-# Create a variable with empty strings
-# Add numbers then take the result of mod 26
-# Add 65 to get the ciphertext
+
+def encrypt (plaintext, key):
+    
+#   Create a variable with empty strings
+    ciphertext = ""
+    for i in range (len(plaintext_char_value)):
+        
+#       Add numbers then take the result of mod 26
+        character_value = (plaintext_char_value [i] + key_char_value [i % key_length]) % 26
+        
+#       Add 65 to get the ciphertext
+        ciphertext += chr (character_value + 65)
+        
+#   Return the value
+    return ciphertext
+
 # Call the function and display the output
+
+print (encrypt(plaintext, key))
 
 
